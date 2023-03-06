d66d1890c8b1ba4a281bb9994988637604ffa48b
maksud9909 <rmaksud822@gmail.com>
2023-03-06 21:07:20 +0500

    Initial commit

---
+++ src/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    public static void main(String[] args) {
+        Test1 t = new Test1();
+        System.out.println(t.n1);
+    }
+    String n1 = new String("Hello world");
+
+}
